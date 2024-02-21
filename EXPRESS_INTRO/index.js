f93b0f82b673bac934d59890ecfc438a869a604b
=Punit2709 <=punitsp2003@gmail.com>
2024-02-21 21:15:56 +0530

    EXRESS_INTRO added

---
+++ EXPRESS_INTRO/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+// middle ware should before routes
+app.set("view engine", 'ejs');
+app.use(express.static('./public'))
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+app.get('/profile/:username', function (req, res) {
+  let name = req.params.username.toUpperCase();
+  res.render(`profile`, { name })
+})
+
+app.get('/contacts', function (req, res) {
+  res.render(`contacts`)
+})
+
+app.get('/error', function (req, res, next) {
+  throw new Error('Path not exists');
+})
+
+app.use(function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.render('error', { error: err })
+})
+
+app.listen(3000, () => console.log('Server Started'))
